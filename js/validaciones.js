ae94b4765ef608a0f9adba24504d48ae3d13f1d8
alestesus <alestesus@gmail.com>
2019-03-04 12:10:46 +0100

    Create validaciones.js

---
+++ js/validaciones.js
@@ -0,0 +1,211 @@
+///ALTAS
+//validar Alta Operario
+function validarOperario(oEvento) {
+
+    var oE = oEvento || window.event;
+    var bValido = true;
+    var sError = "";
+
+    limpiarErrores();
+
+    //Validar DNI del Operario
+    var sDNI = frmAltaOperario.txtDniOp.value.trim();
+    var oExpReg = /^\d{8}[a-zA-Z]$/;
+
+    if (oExpReg.test(sDNI) == false) {
+        bValido = false;
+
+        frmAltaOperario.txtDniOp.classList.add("error");
+        frmAltaOperario.txtDniOp.focus();
+        sError += " El DNI debe contener 8 numeros y una letra final.";
+    }
+	//Validar nombre del Operario
+    var sNombre = frmAltaOperario.txtNombreOp.value.trim();
+    var oExpReg = /^[a-zA-Z0-9\s]{2,50}$/;
+
+    if (oExpReg.test(sNombre) == false) {
+        bValido = false;
+
+        frmAltaOperario.txtNombreOp.classList.add("error");
+        frmAltaOperario.txtNombreOp.focus();
+        sError += "\n El nombre debe ser alfanumérico entre 2 y 50 caracteres.";
+    }
+   
+    if (bValido == false) {
+        // Mostrar errores
+        alert(sError);
+
+        //Cancelar submit
+        oE.preventDefault();
+    }
+
+}
+
+	//Validar nombre del Operario
+    var sNombre = frmAltaAdministrativo.txtNombreAdm.value.trim();
+    var oExpReg = /^[a-zA-Z0-9\s]{2,50}$/;
+
+    if (oExpReg.test(sNombre) == false) {
+        bValido = false;
+
+        frmAltaAdministrativo.txtNombreAdm.classList.add("error");
+        frmAltaAdministrativo.txtNombreAdm.focus();
+        sError += "\n El nombre debe ser alfanumérico entre 2 y 50 caracteres.";
+    }
+   
+    if (bValido == false) {
+        // Mostrar errores
+        alert(sError);
+
+        //Cancelar submit
+        oE.preventDefault();
+    }
+
+}
+
+
+//Validar Alta Cliente
+function validarCliente(oEvento) {
+
+    var oE = oEvento || window.event;
+    var bValido = true;
+    var sError = "";
+
+    limpiarErrores();
+
+    //Validar DNI del Ciente
+    var sDNI = frmAltaCliente.txtDniCli.value.trim();
+    var oExpReg = /^\d{8}[a-zA-Z]$/;
+
+    if (oExpReg.test(sDNI) == false) {
+        bValido = false;
+
+        frmAltaCliente.txtDniCli.classList.add("error");
+        frmAltaCliente.txtDniCli.focus();
+        sError += " El DNI debe contener 8 numeros y una letra final.";
+    }
+	//Validar nombre del cliente
+    var sNombre = frmAltaCliente.txtNombreCli.value.trim();
+    var oExpReg = /^[a-zA-Z0-9\s]{2,50}$/;
+
+    if (oExpReg.test(sNombre) == false) {
+        bValido = false;
+
+        frmAltaCliente.txtNombreCli.classList.add("error");
+        frmAltaCliente.txtNombreCli.focus();
+        sError += "\n El nombre debe ser alfanumérico entre 2 y 50 caracteres.";
+    }
+
+    //Validar Direccion del cliente
+    var sDireccion = frmAltaCliente.txtDireccionCli.value.trim();
+    oExpReg = /^[a-zA-Z0-9\s]{2,50}$/;
+
+    if (oExpReg.test(sDireccion) == false) {
+
+        if (bValido == true) { // ==> Primer error detectado en este campo
+            frmAltaCliente.txtDireccionCli.focus();
+            bValido = false;
+        }
+
+        frmAltaCliente.txtDireccionCli.classList.add("error");
+
+        sError += "\n La direccion es incorrecta.";
+    }
+
+    //Validar telefono del cliente 
+    var sTelefono = frmAltaCliente.txtTfnoCli.value.trim();
+    oExpReg = /^[679]\d{8}$/;
+
+    if (oExpReg.test(sTelefono) == false) {
+
+        if (bValido == true) { // ==> Primer error detectado en este campo
+            frmAltaCliente.txtTfnoCli.focus();
+            bValido = false;
+        }
+
+        frmAltaCliente.txtTfnoCli.classList.add("error");
+
+        sError += "\n El teléfono es incorrecto.";
+    }
+
+    
+    if (bValido == false) {
+        // Mostrar errores
+        alert(sError);
+
+        //Cancelar submit
+        oE.preventDefault();
+    }
+
+}
+
+
+//Validar Alta Material
+function validarAltaMaterial(oEvento) {
+
+    var oE = oEvento || window.event;
+    var bValido = true;
+    var sError = "";
+
+    limpiarErrores();
+
+	//Validar Codigo de Alta Material
+	var sCod = frmAltaMaterial.txtCodMat.value.trim();
+    var oExpReg = /^[a-zA-Z]{3}\d{3}$/;
+
+    if (oExpReg.test(sCod) == false) {
+        bValido = false;
+
+        frmAltaMaterial.txtCodMat.classList.add("error");
+        frmAltaMaterial.txtCodMat.focus();
+        sError += " El Codigo debe empezar por 3 vocales seguido de 3 digitos.";
+    }
+	//Validar nombre de Alta Material
+    var sNombre = frmAltaMaterial.txtNombreMat.value.trim();
+    var oExpReg = /^[a-zA-Z0-9\s]{2,50}$/;
+
+    if (oExpReg.test(sNombre) == false) {
+        bValido = false;
+
+        frmAltaMaterial.txtNombreMat.classList.add("error");
+        frmAltaMaterial.txtNombreMat.focus();
+        sError += "\n El nombre debe ser alfanumérico entre 2 y 50 caracteres.";
+    }
+	//Validar Precio de Alta Material
+	var sPrecio = frmAltaMaterial.txtPrecioMat.value.trim();
+    var oExpReg = /^\d+(\.\d{1,2})?$/;
+
+    if (oExpReg.test(sPrecio) == false) {
+        bValido = false;
+
+        frmAltaMaterial.txtPrecioMat.classList.add("error");
+        frmAltaMaterial.txtPrecioMat.focus();
+        sError += "\n El Precio debe ser numérico y contener como maximo 2 decimales.";
+    }
+	//Validar Descripcion de Alta Material
+	var sDes = frmAltaMaterial.txtDescripcionMat.value.trim();
+    var oExpReg = /^[a-zA-Z0-9\s]{1,50}$/;
+
+    if (oExpReg.test(sDes) == false) {
+        bValido = false;
+
+        frmAltaMaterial.txtDescripcionMat.classList.add("error");
+        frmAltaMaterial.txtDescripcionMat.focus();
+        sError += "\n Añade una descripción.";
+    }
+	
+    if (bValido == false) {
+        // Mostrar errores
+        alert(sError);
+
+        //Cancelar submit
+        oE.preventDefault();
+    }
+
+}
+
+
+///MODIFICAR 
+
+
+
