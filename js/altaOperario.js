2926f4f7032eb097b3ee16f89f287a5a82116c3a
Alvaro1998 <alsaverconv@gmail.com>
2019-03-04 13:00:14 +0100

    A

---
+++ js/altaOperario.js
@@ -0,0 +1,27 @@
+$("#btnAceptarOperario").click(altaOperario);
+
+function altaOperario()
+{
+    validarOperario();
+
+    var oCliente = {
+        DNI: document.getElementById("txtDniOp").value.trim(),
+        Nombre: document.getElementById("txtNombreOp").value.trim()
+    };
+    //  IMPORTANTE: EL NOMBRE DE LOS PARAMETROS ENVIADOS DIFIERE EN EL CASO DEL OBJETO LITERAL
+    var sParametros = "datos=" + JSON.stringify(oCliente);
+
+    $.post("php/altaOperario.php", sParametros, respuestaAltaOperario, 'json');
+
+    function respuestaAltaOperario(oDatos, sStatus, oXHR) 
+    {
+        if (oDatos.error) {
+            alert(oDatos.mensaje);
+        } else {
+            alert(oDatos.mensaje);
+            frmAltaOperario.reset();
+            frmAltaOperario.style.display="none";
+            $("#divGestion").show("normal");
+        }
+    }
+}
---
+++ php/altaOperario.php
@@ -0,0 +1,33 @@
+<?php
+
+// Configuración BASE DE DATOS MYSQL
+$servidor  = "localhost";
+$basedatos = "upobrico";
+$usuario   = "root";
+$password  = "";
+
+// Recojo los datos de entrada
+$datosJSON = $_POST["datos"];
+//Decodifico el objeto operario
+$operario = json_decode($datosJSON);
+
+// Creamos la conexión al servidor.
+$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
+mysqli_set_charset($conexion,"utf8");
+mysqli_query($conexion,"utf8");
+
+$sql = "INSERT INTO operarios (DNI, NOMBRE) VALUES ('$operario->DNI','$operario->Nombre');";
+$resultado = mysqli_query($conexion,$sql);
+
+if ($resultado){
+    $respuesta["error"] = 0;
+    $respuesta["mensaje"] = "Alta realizada"; 
+} else {
+    $respuesta["error"] = 1;
+    $respuesta["mensaje"] = "Error en el proceso de alta: ".mysqli_error($conexion);
+}
+
+echo json_encode($respuesta);
+
+mysqli_close($conexion);
+?>
